647e6b1deb69bab1ed28cb69a851122aa2d03e69
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2011-03-30 14:37:39 +0400

    Sample project for question http://stackoverflow.com/questions/5483187/active-record-problem-with-translation-template-and-body-error-messages-in-yml/5483452#5483452

---
+++ 5483187/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'baa5d16dd1dcd9d82375033bcbae0a2b33b2d3ae0d0f19804a156ca19885f1767fd02e15477c4211f561b076e5f7adc7dc4303d01f3caa6cf7e3dcdbe9f550b9'
